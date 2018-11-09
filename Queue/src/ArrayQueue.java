6e05a9083a289157c3f8d6ba8af5f58424faaab9
1256233771 <1256233771@qq.com>
2018-11-09 08:40:13 +0800

    两种不同队列的性能测试

--- Queue/src/ArrayQueue.java
+++ Queue/src/ArrayQueue.java
@@ -50,5 +50,16 @@ public class ArrayQueue<E> implements Queue{
         res.append("] tail ");
         return res.toString();
     }
+    public static void main(String[] args) {
+        ArrayQueue<Integer> queue = new ArrayQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+        }
+        System.out.println(queue);
+        queue.dequeue();
+        System.out.println(queue);
+        Integer front = queue.getFront();
+        System.out.println(front);
+    }
 
 }
--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,13 +1,29 @@
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        ArrayQueue<Integer> queue = new ArrayQueue<>();
-        for (int i = 0; i < 10; i++) {
-            queue.enqueue(i);
+
+    // 测试使用q运行opCount个enqueue和dequeue操作所需要的时间,单位:秒
+    private static double testQueue(Queue<Integer> q,int opCount){
+        long startTime = System.nanoTime();
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
         }
-        System.out.println(queue);
-        queue.dequeue();
-        System.out.println(queue);
-        Integer front = queue.getFront();
-        System.out.println(front);
+        for (int i = 0; i < opCount; i++) {
+            q.dequeue();
+        }
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+        int opCount = 100000;
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue,opCount);
+        System.out.println("ArrayQueue , time :" + time1 + " s");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue,opCount);
+        System.out.println("LoopQueue , time :" + time2 + " s");
     }
 }
